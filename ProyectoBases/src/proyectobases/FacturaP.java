fac8f244ae4d704c544e587f7c96e31f1de57d7f
Sergio <sergiocardenas14@hotmail.com>
2015-01-13 11:18:57 -0500

    Activacion y desactivacion de campos de texto y botones

--- ProyectoBases/src/proyectobases/FacturaP.java
+++ ProyectoBases/src/proyectobases/FacturaP.java
@@ -10,6 +10,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
 import java.util.LinkedList;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
@@ -32,7 +33,12 @@ public class FacturaP extends JFrame {
     public String precio= "";
     
     
+    JRadioButton op_compra = new JRadioButton("Compra", false);
+    JRadioButton op_venta = new JRadioButton("Venta", false);
     
+    ButtonGroup grupo = new ButtonGroup();
+        
+    JLabel lbl0 = new JLabel("Tipo:");
     JLabel lbl1 = new JLabel("Cliente:");
     JLabel lbl2 = new JLabel("RUC:");
     JLabel lbl3 = new JLabel("Dirección:");
@@ -82,47 +88,57 @@ public class FacturaP extends JFrame {
         setVisible(true);
         setTitle("Facturación");
         setResizable(false);
-        setSize(1024, 634);
+        setSize(512, 634);
         setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+               
         
+        op_compra.setBounds(135,5,70,15);
+        op_compra.setOpaque(false);
+        op_compra.setForeground(Color.white);
+        op_venta.setBounds(210,5,70,15);
+        op_venta.setOpaque(false);
+        op_venta.setForeground(Color.white);
         
-        lbl1.setBounds(25,20,100,15);
+        lbl0.setBounds(25,5,100,15);
+        lbl0.setForeground(Color.white);
+        lbl1.setBounds(25,35,100,15);
         lbl1.setForeground(Color.white);
-        lbl2.setBounds(25,50,100,15);
+        lbl2.setBounds(25,65,100,15);
         lbl2.setForeground(Color.white);
-        lbl3.setBounds(25,80,100,15);
+        lbl3.setBounds(25,95,100,15);
         lbl3.setForeground(Color.white);
-        lbl4.setBounds(25,110,100,15);
+        lbl4.setBounds(25,125,100,15);
         lbl4.setForeground(Color.white);
-        lbl5.setBounds(245,110,100,15);
+        lbl5.setBounds(245,125,100,15);
         lbl5.setForeground(Color.white);
         Font m = new Font("Arial", Font.BOLD, 14);
         lbl6.setFont(m);
-        lbl6.setBounds(205,145,100,15);
+        lbl6.setBounds(205,160,100,15);
         lbl6.setForeground(Color.YELLOW);
-        lbl7.setBounds(25,170,100,15);
+        lbl7.setBounds(25,185,100,15);
         lbl7.setForeground(Color.white);
-        lbl8.setBounds(192,170,100,15);
+        lbl8.setBounds(192,185,100,15);
         lbl8.setForeground(Color.white);
-        lbl9.setBounds(365,170,100,15);
+        lbl9.setBounds(365,185,100,15);
         lbl9.setForeground(Color.white);
         message.setFont(m);
-        message.setBounds(25,255,400,15);
+        message.setBounds(25,270,400,15);
         message.setForeground(Color.ORANGE);
-        lbl10.setBounds(295,490,80,15);
+        lbl10.setBounds(295,505,80,15);
         lbl10.setForeground(Color.white);
         
                         
-        txt1.setBounds(135,15,300,20);
-        txt2.setBounds(135,45,300,20);
-        txt3.setBounds(135,75,300,20);
-        txt4.setBounds(135,105,100,20);
+        txt1.setBounds(135,30,300,20);
+        txt2.setBounds(135,60,300,20);
+        txt3.setBounds(135,90,300,20);
+        txt4.setBounds(135,120,100,20);
         txt4.setEditable(false);
-        txt5.setBounds(305,105,130,20);
-        txt6.setBounds(25,195,100,20);
-        txt7.setBounds(192,195,70,20);
-        txt8.setBounds(365,195,70,20);
-        txt9.setBounds(335,490,95,20);
+        txt5.setBounds(305,120,130,20);
+        txt6.setBounds(25,210,100,20);
+        txt7.setBounds(192,210,70,20);
+        txt8.setBounds(365,210,70,20);
+        txt9.setBounds(335,505,95,20);
         txt9.setEditable(false);
         
         
@@ -137,15 +153,15 @@ public class FacturaP extends JFrame {
         Icon imgBtnFacturar = new ImageIcon(getClass().getResource("/imgs/btnFacturar.jpg"));
         Icon imgBtnRegresar = new ImageIcon(getClass().getResource("/imgs/btnRegresar.jpg"));
         
-        btnCrear.setBounds(25, 220, 100, 25);
+        btnCrear.setBounds(25, 235, 100, 25);
         btnCrear.setIcon(imgBtnCrear);
-        btnModificar.setBounds(175, 220, 100, 25);
+        btnModificar.setBounds(175, 235, 100, 25);
         btnModificar.setIcon(imgBtnModificar);
-        btnQuitar.setBounds(335, 220, 100, 25);
+        btnQuitar.setBounds(335, 235, 100, 25);
         btnQuitar.setIcon(imgBtnQuitar);
         
         
-        btnAgCliente.setBounds(25, 510, 125, 32);
+        btnAgCliente.setBounds(25, 525, 125, 32);
         btnAgCliente.setIcon(imgBtnAgCliente);
         btnAgCliente.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
@@ -157,7 +173,7 @@ public class FacturaP extends JFrame {
                 btnAgClienteActionPerformed(evt);
             }
         });
-        btnAgProduct.setBounds(25, 552, 125, 32);
+        btnAgProduct.setBounds(25, 567, 125, 32);
         btnAgProduct.setIcon(imgBtnAgProduct);
         btnAgProduct.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
@@ -169,7 +185,7 @@ public class FacturaP extends JFrame {
                 btnAgProductActionPerformed(evt);
             }
         });
-        btnFacturar.setBounds(160, 510, 125, 32);
+        btnFacturar.setBounds(160, 525, 125, 32);
         btnFacturar.setIcon(imgBtnFacturar);
         btnFacturar.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
@@ -181,7 +197,7 @@ public class FacturaP extends JFrame {
                 btnFacturarActionPerformed(evt);
             }
         });
-        btnRegresar.setBounds(160, 552, 125, 32);
+        btnRegresar.setBounds(160, 567, 125, 32);
         btnRegresar.setIcon(imgBtnRegresar);
         btnRegresar.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
@@ -201,6 +217,17 @@ public class FacturaP extends JFrame {
         modelo.addColumn("Precio");
         tabAgregados.setModel(modelo);
         
+        op_compra.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                opCompraMouseClicked(evt);
+            }
+        });
+        
+        op_venta.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                opVentaMouseClicked(evt);
+            }
+        });
                 
         txt1.addFocusListener(new java.awt.event.FocusAdapter() {
             public void focusLost(java.awt.event.FocusEvent evt) {
@@ -264,6 +291,39 @@ public class FacturaP extends JFrame {
             }
         });
         
+        txt6.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                txt6FocusLost(evt);
+            }
+        });
+        txt6.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txt6ActionPerformed(evt);
+            }
+        });
+        
+        txt7.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                txt7FocusLost(evt);
+            }
+        });
+        txt7.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txt7ActionPerformed(evt);
+            }
+        });
+        
+        txt8.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                txt8FocusLost(evt);
+            }
+        });
+        txt8.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txt8ActionPerformed(evt);
+            }
+        });
+        
         
         /*Revisar!!!!!!!!!!!!!!!!!!!!!*/
 //        tabAgregados.setModel(new javax.swing.table.DefaultTableModel(
@@ -299,11 +359,18 @@ public class FacturaP extends JFrame {
         
         tabAgregados.setBackground(Color.white);
 //        tabAgregados.setBounds(30, 285, 399, 200);
-        sp1.setBounds(30, 270, 399, 200);
+        sp1.setBounds(30, 285, 399, 200);
         
         
         Panel p = new Panel();
-                                 
+        
+        grupo.add(op_compra);
+        grupo.add(op_venta);
+        
+        p.add(op_compra);
+        p.add(op_venta);
+        
+        p.add(lbl0);
         p.add(lbl1);
         p.add(lbl2);
         p.add(lbl3);
@@ -340,6 +407,29 @@ public class FacturaP extends JFrame {
         p.setLayout(new BorderLayout(0, 0));
         setContentPane(p);
         
+    }
+    
+    
+    public void opCompraMouseClicked(java.awt.event.MouseEvent evt){
+        if(op_compra.isSelected() == true){
+            txt6.setEditable(false);
+            txt7.setEditable(false);
+            txt8.setEditable(false);
+            btnCrear.setEnabled(false);
+            btnModificar.setEnabled(false);
+            btnQuitar.setEnabled(false);
+        }
+    }
+    
+    public void opVentaMouseClicked(java.awt.event.MouseEvent evt){
+        if(op_venta.isSelected() == true){
+            txt6.setEditable(true);
+            txt7.setEditable(true);
+            txt8.setEditable(true);
+            btnCrear.setEnabled(true);
+            btnModificar.setEnabled(true);
+            btnQuitar.setEnabled(true);
+        }
     }
     
     private void txt1ActionPerformed(java.awt.event.ActionEvent evt) {                                            
@@ -477,14 +567,58 @@ public class FacturaP extends JFrame {
     private void btnFacturarActionPerformed(java.awt.event.ActionEvent evt) {                                           
         // TODO add your handling code here:
     }
-    
+        
     private void btnRegresarMouseClicked(java.awt.event.MouseEvent evt) {                                        
         // TODO add your handling code here:
-        dispose();
+        this.dispose();
+        new Menu().show();
     }
     private void btnRegresarActionPerformed(java.awt.event.ActionEvent evt) {                                           
         // TODO add your handling code here:
     }  
+    
+    private void txt6ActionPerformed(java.awt.event.ActionEvent evt) {                                                 
+        // TODO add your handling code here:
+        nombre = evt.getActionCommand();
+        txt6.setForeground(java.awt.Color.RED);
+        if (ValidateInputEmpleado.validateProductName(nombre)){
+            //System.out.println( "Valid input.  Thank you." );
+            txt6.setForeground(java.awt.Color.BLACK);
+        }
+    }      
+    
+    private void txt6FocusLost(java.awt.event.FocusEvent evt) {                                           
+        // TODO add your handling code here:
+    }
+    
+    private void txt7ActionPerformed(java.awt.event.ActionEvent evt) {                                               
+        // TODO add your handling code here:
+        cantidad = evt.getActionCommand();
+        txt7.setForeground(java.awt.Color.RED);
+        if (ValidateInputEmpleado.validateCantidad(cantidad)){
+            //System.out.println( "Valid input.  Thank you." );
+            txt7.setForeground(java.awt.Color.BLACK);
+        }
+    }                                              
+
+    private void txt7FocusLost(java.awt.event.FocusEvent evt) {                                         
+        // TODO add your handling code here:
+    }  
+   
+    
+    private void txt8ActionPerformed(java.awt.event.ActionEvent evt) {                                                 
+        // TODO add your handling code here:
+        precio = evt.getActionCommand();
+        txt8.setForeground(java.awt.Color.RED);
+        if (ValidateInputEmpleado.validatePrecio(precio)){
+            //System.out.println( "Valid input.  Thank you." );
+            txt8.setForeground(java.awt.Color.BLACK);
+        }
+    }                                                
+
+    private void txt8FocusLost(java.awt.event.FocusEvent evt) {                                           
+        // TODO add your handling code here:
+    } 
     
     class Panel extends javax.swing.JPanel {
         public Panel(){
